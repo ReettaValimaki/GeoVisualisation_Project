8bd8cbe6bc2a38ed4842b47a59a18bd879574fed
valimaki <reetta.vaelimaeki@uzh.ch>
2019-04-04 21:56:24 +0200

    some stuff

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+
+	<title>PHP Test</title>
+</head>
+<body>
+Hello world!
+<?php
+	$myvar = "This is my variable";
+	echo "Hello World!";
+	echo $myvar
+	$number = 5;
+	$number2 = 2;
+	$sum = $number + $number2;
+	echo $sum;
+	$name = "Bob";
+	echo "Hello" . $name;
+
+?>
+</body>
+</html>
+
